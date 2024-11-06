50b0055d681f9d19e3a32392bd1e97c35251bc5f
Alef5750 <alewitt@gmail.com>
2024-11-06 17:00:26 +0200

    color-coded notes

--- src/components/Note.tsx
+++ src/components/Note.tsx
@@ -1,18 +1,28 @@
-import { INoteFormData } from "../types";
+import { CategoryEnum, INoteFormData } from "../types";
 
 interface IProps {
   note: INoteFormData;
 }
+
+const colorBasedOnCategory = (category: CategoryEnum) => {
+  let color = "lightblue";
+  if (category === CategoryEnum.PERSONAL) color = "lightcoral";
+  if (category === CategoryEnum.WORK) color = "yellow";
+  if (category === CategoryEnum.SHOPPING) color = "orange";
+  const styleObj = {
+    border: "3px solid darkgreen",
+    backgroundColor: color,
+    padding: "5px",
+    margin: "10px",
+    maxWidth: "500px",
+    cursor: "pointer",
+  };
+  return styleObj;
+};
+
 export const Note = ({ note }: IProps) => {
   return (
-    <div
-      style={{
-        border: "3px solid darkgreen",
-        padding: "5px",
-        maxWidth: "500px",
-        cursor: "pointer",
-      }}
-    >
+    <div style={colorBasedOnCategory(note.category)}>
       <h2>{note.title}</h2>
       <h3>ID: {note.id}</h3>
       <p>{note.content}</p>
